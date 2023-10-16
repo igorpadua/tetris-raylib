0553514ffb98d679505b15dccb17402b36cf154b
igorpadua <igorpadua@tutanota.com>
2023-10-16 14:51:16 -0300

    feat: create window

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+    InitWindow(300, 600, "Raylib Tetris");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
