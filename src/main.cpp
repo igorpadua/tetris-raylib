b241ce4a7aff0539afad69ed300d21d55bfc23ad
igorpadua <igorpadua@tutanota.com>
2023-10-16 14:56:28 -0300

    feat: background blue

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,13 @@
 
 int main()
 {
+    Color darkBlue = { 44, 44, 127, 255 };
     InitWindow(300, 600, "Raylib Tetris");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-
+        ClearBackground(darkBlue);
         EndDrawing();
     }
 
